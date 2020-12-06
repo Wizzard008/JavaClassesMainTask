73a8c32a3e75b108e5ce4ace14a25f5197fd5e7f
Vladimir <ryzhenkov_v@list.ru>
2020-12-06 16:53:24 +1100

    Start work on Java Classes>Main Task

---
+++ src/MainTask/Student.java
@@ -0,0 +1,16 @@
+package MainTask;
+
+public class Student {
+    private int id;
+    private String name;
+    private String surname;
+    private String middleName;
+    private int yearBirth;
+    private String adress;
+    private String phoneNumber;
+    private String faculty;
+    private String group;
+    private int course;
+
+
+}
